c81503121f80a974de7dc9d4be674c236458ad48
Maik Luiz Paixão <paixomaik@outlook.com.br>
2020-09-07 15:56:44 -0300

    folder structure created

---
+++ benchmark/dataset.sh
@@ -0,0 +1,3 @@
+wget -P bert-base-uncased https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/bert-base-uncased/config.json
+wget -P bert-base-uncased https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/bert-base-uncased/pytorch_model.bin
+wget -P bert-base-uncased https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/bert-base-uncased/vocab.txt
---
+++ scripts/train.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+def main():
+        print("Start...")
+
+if "__name__" == "__main__":
+        main()
