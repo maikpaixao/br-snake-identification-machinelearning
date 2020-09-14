d6eb0382c59d6dc1e3ca0e49fc4d96fe5bd5681f
Maik Luiz Paixão <paixomaik@outlook.com.br>
2020-09-13 21:14:36 -0300

    resize method implemented

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.utils import Bunch
 from skimage.io import imread
 from skimage.transform import resize
+from PIL import Image, ImageOps
 
 class Preprocess:
         def __init__(self):
@@ -12,6 +13,16 @@ class Preprocess:
                 self.images = ""
                 self.flat_data = ""
 
+        def resize_image_files(self, container_path):
+                size = 200, 200
+                image_dir = Path(container_path)
+                folders = [directory for directory in image_dir.iterdir() if directory.is_dir()]
+
+                for i, direc in enumerate(folders):
+                        for file in direc.iterdir():
+                                im = Image.open(file)
+                                im = ImageOps.fit(im, size, Image.ANTIALIAS)
+                                im.save(str(file), "JPEG")
 
         def loadfile(self, container_path, dimension = (200, 200, 3)):
                 image_dir = Path(container_path)
